b12785ec34e32bccec42c2962da1905371d41b79
Ahoudori77 <rakurusu77@gmail.com>
2024-10-22 16:32:21 +0900

    ユーザー管理機能-修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龠 々ーa-zA-Z0-9]+\z/, message: 'はひらがな、カタカナ、漢字、英数字のみ使用できます' }
-  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: 'は有効なメールアドレスを入力してください' }
-  with_options presence: true, format: { with: /\A[一-龠ぁ-んァ-ン]+\z/, message: 'は全角文字のみ使用できます' } do
+  validates :nickname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ーa-zA-Z0-9\s]+\z/, message: 'はひらがな、カタカナ、漢字、英数字のみ使用できます' }
+  with_options presence: true, format: { with: /\A[一-龥々]+\z/, message: 'は全角文字のみ使用できます' } do
     validates :last_name
     validates :first_name
   end
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カタカナのみ使用できます' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'は全角カタカナのみ使用できます' }
-
-  PASSWORD_REGEX = /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/
-  validates :password, presence: true, format: { with: PASSWORD_REGEX, message: 'は6文字以上で、英字と数字を両方含める必要があります' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナのみ使用できます' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は全角カタカナのみ使用できます' }
 
   validates :birthday, presence: true
 
-  has_many :items
-  has_many :purchases
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,31 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'バリデーション' do
-    let(:user) { build(:user) }
+  describe 'ユーザーバリデーションのテスト' do
+    subject { build(:user) }
 
-    it '有効なユーザー' do
-      expect(user).to be_valid
+    context 'ユーザーの登録が有効な場合' do
+      it '全ての項目が正しく入力されていれば登録できる' do
+        expect(subject).to be_valid
+      end
     end
 
-    it 'ニックネームが必須' do
-      user.nickname = nil
-      expect(user).to_not be_valid
-    end
+    context 'バリデーションが発生する場合' do
+      it 'ニックネームが空では登録できない' do
+        subject.nickname = nil
+        expect(subject).to_not be_valid
+      end
 
-    it 'メールアドレスが必須' do
-      user.email = nil
-      expect(user).to_not be_valid
-    end
+      it 'メールアドレスが空では登録できない' do
+        subject.email = nil
+        expect(subject).to_not be_valid
+      end
 
-    it 'パスワードが必須' do
-      user.password = nil
-      expect(user).to_not be_valid
-    end
+      it 'パスワードが空では登録できない' do
+        subject.password = nil
+        expect(subject).to_not be_valid
+      end
 
-    it '生年月日が必須' do
-      user.birthday = nil
-      expect(user).to_not be_valid
+      it '生年月日が空では登録できない' do
+        subject.birthday = nil
+        expect(subject).to_not be_valid
+      end
+
+      it '苗字が全角以外だと登録できない' do
+        subject.last_name = 'Tomato'
+        expect(subject).to_not be_valid
+      end
+
+      it '名前が全角以外だと登録できない' do
+        subject.first_name = 'Daisuki'
+        expect(subject).to_not be_valid
+      end
+
+      it '苗字のフリガナがカタカナ以外だと登録できない' do
+        subject.last_name_kana = 'とまと'
+        expect(subject).to_not be_valid
+      end
+
+      it '名前のフリガナがカタカナ以外だと登録できない' do
+        subject.first_name_kana = 'だいすき'
+        expect(subject).to_not be_valid
+      end
     end
   end
 end
