2a4a61ecf7ee0e6d695d2a55dd083d5a85ea9463
Ahoudori77 <rakurusu77@gmail.com>
2024-10-23 15:52:55 +0900

    カテゴリ作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,13 @@ class Item < ApplicationRecord
   belongs_to_active_hash :condition
   belongs_to_active_hash :shipping_fee
   belongs_to_active_hash :prefecture
-  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :delivery_time
+
+  validates :category_id, numericality: { other_than: 1, message: "を選択してください" }
+  validates :condition_id, numericality: { other_than: 1, message: "を選択してください" }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "を選択してください" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "を選択してください" }
+  validates :delivery_time_id, numericality: { other_than: 1, message: "を選択してください" }
 
   validates :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :price, :image, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
